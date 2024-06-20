526b4c400ce342d14203c5fab686e2d0c9e6f5fc
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-06-20 16:25:17 +0100

    Add 6.14 Anecdotes and the Backend, step 1

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -2,9 +2,17 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteServie from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 const App = () => {
-
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteServie
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [])
   return (
     <div>
       <h2>Anecdotes</h2>
